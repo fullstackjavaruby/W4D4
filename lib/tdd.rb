92d6fe36bb7f17a52f75e784f5e45855f560b047
kakintse <kinkatse@mail.adelphi.edu>
2021-05-13 17:18:24 -0400

    Completed Stock_profit

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -21,11 +21,28 @@ end
 def my_transpose(arr)
     new_arr = Array.new(arr.length) {Array.new([])}
     arr.each_with_index do |row, i1|
-        arr.each_with_index do |col, i2|
+        row.each_with_index do |col, i2|
             new_arr[i2] << row[i2]
         end
     end
     new_arr
 end
 
-
+def stock_picker(arr)
+    stock_profit = Hash.new {|h,k| h[k] = []}
+    arr.each_with_index do |buy, buy_day|
+        arr.each_with_index do |sell, sell_day|
+            if sell_day > buy_day
+                stock_profit[sell-buy] << [buy_day, sell_day]
+            end
+            # stock_profit[buy_day] << sell-buy
+        end
+    end
+    p stock_profit
+    if !stock_profit.keys.any? {|n| n > 0}
+        return "Should not buy"
+    else
+        stock_values = stock_profit.sort_by {|k,v| k}
+        return stock_values[-1][-1]
+    end
+end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -18,8 +18,40 @@ describe "#my_transpose" do
         [1, 4, 7],
         [2, 5, 8]  
     ]}
+    let(:arr2) {[
+        [a, b, c]
+    ]}
+    let(:arr3) {[
+        [a, nil, nil],
+        [b, nil, nil],
+        [c, nil, nil]
+    ]}
 
     it "will convert matrix to transposed version" do
         expect(my_transpose(arr1)).to eq(arr1.transpose)
-    end    
+    end
+
+    # it "will put nil in place where there is not enough spaces to fill a square array" do
+    #     expect(my_transpose(arr2)).to eq(arr3)
+    # end
+end
+
+describe "#stock_picker" do
+    let(:arr1) {[5,8,11,17,21,23,20]}
+    let(:arr2) {[20,19,18,14,10,8,4]}
+    let(:arr3) {[15,20,8,9,13,8,20]}
+    # most prof to sell is max and most prof to buy is min
+    it "outputs most profitable pair of days to buy and sell" do
+        expect(stock_picker(arr1)).to eq([[0,5]]) # days
+    end
+
+    it "outputs most profitable pair of days to buy and sell pt. 2" do
+        expect(stock_picker(arr3)).to eq([[2,6],[5,6]]) # days
+    end
+
+    # only decrease in price edge case where no profit
+
+    it "considers edge case where profit is only decreasing" do
+        expect(stock_picker(arr2)).to eq("Should not buy") # days
+    end
 end
